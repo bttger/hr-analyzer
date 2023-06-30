270cf2942d8de87e47be6558858c4558c1320456
Tom Boettger <t.boettger@live.de>
2023-06-30 19:02:22 +0200

    Move away from attaching data and functions to html elem

--- popup.js
+++ popup.js
@@ -1,60 +1,48 @@
-const statisticsElem = document.getElementById("statistics");
-
-statisticsElem.data = {
-  maxHR: null,
-  restingHR: null,
-  heartRates: [180, 178, 145, 144, 136, 145, 146, 157],
-  stats: {
-    maximum: null,
-    minimum: null,
-    average: null,
-    median: null,
-    q3: null,
-    q1: null,
-  },
+let maxHR = null;
+let restingHR = null;
+let heartRates = [{ timestamp: 1688133600011, value: 139 }];
+let stats = {
+  maximum: null,
+  minimum: null,
+  average: null,
+  median: null,
+  q3: null,
+  q1: null,
 };
 
 document.getElementById("maxHR").addEventListener("input", (event) => {
-  statisticsElem.data.maxHR = event.target.value;
-  statisticsElem.plotHeartRateZones();
+  maxHR = event.target.value;
+  plotHeartRateZones();
 });
 
 document.getElementById("restingHR").addEventListener("input", (event) => {
-  statisticsElem.data.restingHR = event.target.value;
-  statisticsElem.plotHeartRateZones();
+  restingHR = event.target.value;
+  plotHeartRateZones();
 });
 
-statisticsElem.calculateStats = function () {
-  const values = this.data.heartRates.map((hr) => hr.value);
-  values.sort((a, b) => a - b);
+function calculateStats() {
+  const values = heartRates.map((hr) => hr.value).sort((a, b) => a - b);
   const len = values.length;
-  const q1 = values[Math.floor(len / 4)];
-  const median = values[Math.floor(len / 2)];
-  const q3 = values[Math.floor((3 * len) / 4)];
-  const sum = values.reduce((a, b) => a + b, 0);
-  const average = sum / len;
-  this.data.stats = {
-    maximum: Math.max(...values),
-    minimum: Math.min(...values),
-    average: average.toFixed(2),
-    median: median,
-    q3: q3,
-    q1: q1,
-  };
-  this.updateStatisticsInDOM();
-};
+  stats.q1 = values[Math.floor(len / 4)];
+  stats.median = values[Math.floor(len / 2)];
+  stats.q3 = values[Math.floor((3 * len) / 4)];
+  stats.maximum = Math.max(...values);
+  stats.minimum = Math.min(...values);
+  stats.average = (values.reduce((a, b) => a + b, 0) / len).toFixed(2);
 
-statisticsElem.updateStatisticsInDOM = function () {
-  document.getElementById("maximum").innerText = this.data.stats.maximum;
-  document.getElementById("minimum").innerText = this.data.stats.minimum;
-  document.getElementById("average").innerText = this.data.stats.average;
-  document.getElementById("median").innerText = this.data.stats.median;
-  document.getElementById("q3").innerText = this.data.stats.q3;
-  document.getElementById("q1").innerText = this.data.stats.q1;
-};
+  updateStatisticsInDOM();
+}
 
-statisticsElem.plotRawHeartRate = function () {
-  const heartRates = this.data.heartRates;
+function updateStatisticsInDOM() {
+  document.getElementById("maximum").innerText = stats.maximum;
+  document.getElementById("minimum").innerText = stats.minimum;
+  document.getElementById("average").innerText = stats.average;
+  document.getElementById("median").innerText = stats.median;
+  document.getElementById("q3").innerText = stats.q3;
+  document.getElementById("q1").innerText = stats.q1;
+}
+
+function plotRawHeartRate() {
   const timestamps = heartRates.map((hr) => hr.timestamp);
   const values = heartRates.map((hr) => hr.value);
 
@@ -66,11 +54,13 @@ statisticsElem.plotRawHeartRate = function () {
       mode: "lines",
     },
   ];
-  Plotly.newPlot("raw-heart-rate", data);
-};
 
-statisticsElem.plotBoxPlot = function () {
-  const values = this.data.heartRates.map((hr) => hr.value);
+  Plotly.newPlot("raw-heart-rate", data);
+}
+
+function plotBoxPlot() {
+  const values = heartRates.map((hr) => hr.value);
+
   const data = [
     {
       y: values,
@@ -80,17 +70,17 @@ statisticsElem.plotBoxPlot = function () {
       type: "box",
     },
   ];
-  Plotly.newPlot("boxplot", data);
-};
 
-statisticsElem.plotHeartRateZones = function () {
-  if (this.data.maxHR && this.data.restingHR) {
-    const heartRates = this.data.heartRates.map((hr) => hr.value);
-    const maxHR = this.data.maxHR;
-    const restingHR = this.data.restingHR;
+  Plotly.newPlot("boxplot", data);
+}
+
+function plotHeartRateZones() {
+  if (maxHR && restingHR) {
+    const values = heartRates.map((hr) => hr.value);
     const zones = [0, 0, 0, 0, 0];
-    heartRates.forEach((hr) => {
-      const percentage = ((hr - restingHR) / (maxHR - restingHR)) * 100;
+
+    values.forEach((value) => {
+      const percentage = ((value - restingHR) / (maxHR - restingHR)) * 100;
       if (percentage < 60) zones[0]++;
       else if (percentage < 70) zones[1]++;
       else if (percentage < 80) zones[2]++;
@@ -108,7 +98,7 @@ statisticsElem.plotHeartRateZones = function () {
 
     Plotly.newPlot("heart-rate-zones", data);
   }
-};
+}
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "displayData") {
@@ -116,7 +106,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     const xmlDoc = parser.parseFromString(request.data, "text/xml");
 
     const trackpoints = xmlDoc.getElementsByTagName("Trackpoint");
-    const heartRates = Array.from(trackpoints).map((trackpoint) => {
+    heartRates = Array.from(trackpoints).map((trackpoint) => {
       const timestamp = trackpoint.getElementsByTagName("Time")[0].textContent;
       const value = Number(
         trackpoint.getElementsByTagName("Value")[0].textContent
@@ -124,10 +114,9 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       return { timestamp, value };
     });
 
-    statisticsElem.data.heartRates = heartRates;
-    statisticsElem.calculateStats();
-    statisticsElem.plotRawHeartRate();
-    statisticsElem.plotBoxPlot();
-    statisticsElem.plotHeartRateZones();
+    calculateStats();
+    plotRawHeartRate();
+    plotBoxPlot();
+    plotHeartRateZones();
   }
 });
